0a8bad8e294bf8a35f8ef75c74e517111a25b295
RodrigoCaggiano <88805117+RodrigoCaggiano@users.noreply.github.com>
2021-10-07 21:44:54 -0300

    Update cartao.sol

--- cartao.sol
+++ cartao.sol
@@ -8,7 +8,7 @@ contract CartaoDeVacinacao{
     uint public CNES = 112; 
     string public data = "12/06/2021";
     uint private lote01 = 10;
-    string constant private NomeFabricante = "Janssen"; 
+    string constant private NomeFabricante = "Astrazenica"; 
     uint public CPF = 21916279864; 
     string  public NomeVacinador = "Jose da Silva";
     address private wallet = 0x20Ea0169A674d78d63Fd72748382cFeA01a17aF6;
