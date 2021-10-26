fed5a7b3a123363fae6fd7325c173ec5cfb2ede2
Rodrigo Caggiano <88805117+RodrigoCaggiano@users.noreply.github.com>
2021-10-26 19:49:46 -0300

    Update CompraVenda-oficial.sol
    Versão atualizada

--- CompraVenda-oficial.sol
+++ CompraVenda-oficial.sol
@@ -1,32 +1,34 @@
 pragma solidity 0.8.9;
-
+ 
 contract CompraVenda {
-    
-    string comprador;
-    string vendedor; 
-    
+
+    address public comprador;
+    address public vendedor; 
+
     string public matricula; 
     string public cartorio;
-    
+
     string dataDeVencimento;
-    
+
     bool quitado = false;
-    
+
     uint public valorTotal;
     uint public valorDaEntrada;
     uint public quantidadeDeParcelas;
     uint public porcentagemDaMulta; 
     uint public valorDaParcela;
     uint public valorEmAberto;
-    
+
     constructor(
         uint _valorTotal,
         uint _valorDaEntrada,
         uint _quantidadeDeParcelas,
         string memory _matricula,
-        string memory _cartorio
+        string memory _cartorio,
+        address _vendedor
         ) 
     {
+        vendedor = _vendedor;
         valorTotal = _valorTotal;
         valorDaEntrada = _valorDaEntrada;
         quantidadeDeParcelas = _quantidadeDeParcelas;
@@ -35,25 +37,31 @@ contract CompraVenda {
         valorEmAberto = valorTotal;
         valorDaParcela = funcaoValorParcela();
     }
-    
-    function pagarEntrada(uint _valorPagamento) public returns (uint, string memory) { 
+
+    function pagarEntrada(uint _valorPagamento) public returns (uint, string memory) {
+        require(_valorPagamento == valorDaEntrada, "Valor da entrada incorreto.");
+        require(valorEmAberto == valorTotal, "Entrada ja foi paga.");
+        comprador = msg.sender;
         valorEmAberto = valorTotal - _valorPagamento;
         return(valorEmAberto, "valor em aberto");
     }
-    
-    function pagarParcela(uint _valorDaParcela) public returns (uint, string memory) {
-        valorEmAberto = valorEmAberto - _valorDaParcela;
+
+    function pagarParcela(uint _valorPagamento) public returns (uint, string memory) {
+        require(_valorPagamento == valorDaParcela, "Valor da parcela incorreto");
+        require(valorEmAberto <= valorTotal-valorDaEntrada, "Entrada nao foi foi paga.");
+        require(comprador == msg.sender, "Obrigado, somente o comprador pode executar essa funcao");
+        valorEmAberto = valorEmAberto - _valorPagamento;
         return(valorEmAberto, "valor em aberto");
     }
-    
+
     function funcaoValorParcela() public view returns (uint){
         uint calculoValorParcela = (valorTotal-valorDaEntrada)/quantidadeDeParcelas;
         return(calculoValorParcela);
     }
-    
+
     function valorDaMulta(uint _porcentagemDaMulta) public view returns(uint, string memory) {
         uint multa = _porcentagemDaMulta*valorTotal/100;
         return(multa, "valor da multa");
     }
-    
+
 }
