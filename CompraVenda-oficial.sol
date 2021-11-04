35a53ae8953c67f09e66282263341538e634cfd8
Rodrigo Caggiano <88805117+RodrigoCaggiano@users.noreply.github.com>
2021-11-04 19:42:03 -0300

    Update CompraVenda-oficial.sol

--- CompraVenda-oficial.sol
+++ CompraVenda-oficial.sol
@@ -1,24 +1,26 @@
-pragma solidity 0.8.9;
- 
-contract CompraVenda {
+// SPDX-License-Identifier: MIT
 
+pragma solidity 0.8.9;
+
+contract CompraVenda {
+    
     address public comprador;
     address public vendedor; 
-
+    
     string public matricula; 
     string public cartorio;
-
-    string dataDeVencimento;
-
+    
+    uint public dataDeVencimento;
+    
     bool quitado = false;
-
+    
     uint public valorTotal;
     uint public valorDaEntrada;
     uint public quantidadeDeParcelas;
     uint public porcentagemDaMulta; 
     uint public valorDaParcela;
     uint public valorEmAberto;
-
+    
     constructor(
         uint _valorTotal,
         uint _valorDaEntrada,
@@ -26,7 +28,7 @@ contract CompraVenda {
         string memory _matricula,
         string memory _cartorio,
         address _vendedor
-        ) 
+        )
     {
         vendedor = _vendedor;
         valorTotal = _valorTotal;
@@ -37,31 +39,37 @@ contract CompraVenda {
         valorEmAberto = valorTotal;
         valorDaParcela = funcaoValorParcela();
     }
-
-    function pagarEntrada(uint _valorPagamento) public returns (uint, string memory) {
-        require(_valorPagamento == valorDaEntrada, "Valor da entrada incorreto.");
+    
+    function pagarEntrada() public payable returns (uint, string memory) {
+        require(msg.value == valorDaEntrada, "Valor da entrada incorreto.");
         require(valorEmAberto == valorTotal, "Entrada ja foi paga.");
         comprador = msg.sender;
-        valorEmAberto = valorTotal - _valorPagamento;
+        payable(vendedor).transfer(msg.value);
+        valorEmAberto = valorTotal - msg.value;
+        dataDeVencimento = block.timestamp + 31 * 86400;
         return(valorEmAberto, "valor em aberto");
     }
-
-    function pagarParcela(uint _valorPagamento) public returns (uint, string memory) {
-        require(_valorPagamento == valorDaParcela, "Valor da parcela incorreto");
+    
+    function pagarParcela() public payable returns (uint, string memory) {
+        require(msg.value == valorDaParcela, "Valor da parcela incorreto");
         require(valorEmAberto <= valorTotal-valorDaEntrada, "Entrada nao foi foi paga.");
         require(comprador == msg.sender, "Obrigado, somente o comprador pode executar essa funcao");
-        valorEmAberto = valorEmAberto - _valorPagamento;
+        require(block.timestamp <= dataDeVencimento, "Parcela com data de vencimento vencida");
+        payable(vendedor).transfer(msg.value);
+        dataDeVencimento = dataDeVencimento + 31 * 86400;
+        valorEmAberto = valorEmAberto - msg.value;
         return(valorEmAberto, "valor em aberto");
     }
-
+    
     function funcaoValorParcela() public view returns (uint){
         uint calculoValorParcela = (valorTotal-valorDaEntrada)/quantidadeDeParcelas;
         return(calculoValorParcela);
     }
-
+    
     function valorDaMulta(uint _porcentagemDaMulta) public view returns(uint, string memory) {
+        require(comprador == msg.sender || vendedor == msg.sender, "Apenas o comprador ou vendedor podem executar");
         uint multa = _porcentagemDaMulta*valorTotal/100;
         return(multa, "valor da multa");
     }
-
+    
 }
