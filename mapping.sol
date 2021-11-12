f1dc952156d43988c7690266e22f902b91f4a997
Rodrigo Caggiano <88805117+RodrigoCaggiano@users.noreply.github.com>
2021-11-11 21:38:31 -0300

    Create mapping.sol

---
+++ mapping.sol
@@ -0,0 +1,31 @@
+[21:27] flaviogouveaneto (Guest)
+// SPDX-License-Identifier: MIT pragma solidity 0.8.9; contract Registro {​​​​​
+
+    mapping(uint => string) registroDeNomes;
+
+    function registrarNome(uint _chave, string memory _valor) public {​​​​​
+
+        registroDeNomes[_chave] = _valor;       
+
+    }​​​​​
+
+    function verRegistro(uint _chave) public view returns(string memory) {​​​​​
+
+        return registroDeNomes[_chave];
+
+    }​​​​​
+
+    function editarNome(uint _chave, string memory _novoValor) public {​​​​​
+
+        registroDeNomes[_chave] = _novoValor;
+
+    }​​​​​
+
+    function deletarNome(uint _chave) public {​​​​​
+
+        registroDeNomes[_chave] = "";
+
+    }​​​​​
+
+}​​​​​ 
+
